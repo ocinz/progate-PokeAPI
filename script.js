42702d2b3a4696f61b33de7ae66547505da53327
Ahmad Hafid <86761277+ocinz@users.noreply.github.com>
2023-03-20 21:29:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,23 +10,27 @@ function getPika() {
       const image = data.sprites.front_default;
       const type = data.types[0].type.name;
 
-      //Creating elements
-      const body = document.getElementsByTagName("body")[0];
-      const wrapper = document.createElement("div");
-      const head = document.createElement("h2");
-      const img = document.createElement("img");
-      const typePoke = document.createElement("p");
+      const wrapChecker = document.getElementsByTagName("div")[0];
 
-      //Update element content
-      wrapper.id = "wrapper";
-      img.src = image;
-      head.innerText = `${id}: ${name}`;
-      typePoke.innerText = `Type: ${type}`;
+      if (wrapChecker == undefined) {
+        //Creating elements
+        const body = document.getElementsByTagName("body")[0];
+        const wrapper = document.createElement("div");
+        const head = document.createElement("h2");
+        const img = document.createElement("img");
+        const typePoke = document.createElement("p");
 
-      //Add element to wrapper
-      body.appendChild(wrapper);
-      wrapper.appendChild(head);
-      wrapper.appendChild(img);
-      wrapper.appendChild(typePoke);
+        //Update element content
+        wrapper.id = "wrapper";
+        img.src = image;
+        head.innerText = `${id}: ${name}`;
+        typePoke.innerText = `Type: ${type}`;
+
+        //Add element to wrapper
+        body.appendChild(wrapper);
+        wrapper.appendChild(head);
+        wrapper.appendChild(img);
+        wrapper.appendChild(typePoke);
+      }
     });
 }
